668f92a7d115f97b7d3542ace731c799d44c2bcc
Mashanz <mangatkk@gmail.com>
2022-01-26 10:27:39 +0700

    [ADD] readmi, cli commands and arguments

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
-console.log(`
-KBBI CLI
-`);
+
+const args = process.argv.slice(2);
+
+if (args[0] === "-h" || args.length === 0) {
+  console.log(`
+    Usage:
+
+        kbbi-cli <cari_kata>
+
+    options:
+
+        -h: tampilkan bantuan
+        -v: tampilkan versi
+    `);
+} else if (args[0] === "-v") {
+  console.log(require("./package").version);
+} else {
+  console.log(`
+    🔎 ${args[0]}
+
+    Hasil pencarian: -
+  `);
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+# KBBI CLI
+
+## basic usage
+
+```
+npx kbbi-cli <cari_kata>
+```
+
+## install KBBI CLI
+
+```
+npm -i -g kbbi-cli
+```
+
+## search for a word in KBBI
+
+```
+kbbi-cli <word>
+```
+
+## check kbbi-cli version
+
+```
+kbbi-cli -v
+```
